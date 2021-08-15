9ba793564dd8ac84f3d6dd48b19e5dbb61c7f6f2
Maneesh <thouti.maneesh43@gmail.com>
2021-08-15 08:05:29 -0700

    Contact page added

--- src/components/semantic-components/Footer.js
+++ src/components/semantic-components/Footer.js
@@ -1,23 +1,27 @@
 import React from 'react'
+import { TiLocation ,TiMail,TiPhone} from "react-icons/ti";
 
 const Footer = () => {
     return (
+        <>
       <div className="footer">
         <div className="address">
           <p>Find us at</p>
+            <TiLocation/>
           <address>
             Suite 678, 7360 - 137 Street Surrey, BC, V3W 1A3, Canada.
           </address>
         </div>
         <div className="emailus">
           <p>Email</p>
-          <a href="mailto: info@itextech.ca">info@itextech.ca</a>
+          <a href="mailto: info@itextech.ca"><TiMail/> info@itextech.ca</a>
         </div>
         <div className="contactus">
           <p>Reach us at</p>
-          <a href="tel:+1 (604)628-1988">+1 (604)628-1988</a>
+          <a href="tel:+1 (604)628-1988"><TiPhone/> +1 (604)628-1988</a>
         </div>
       </div>
+            </>
     );
 }
 
