618d9842a3696d72e68ee80d0322761f77c518ab
Maneesh <thouti.maneesh43@gmail.com>
2021-10-30 19:50:30 -0700

    Font,Colorscheme changes

--- src/components/sub-components/Services/Servicecard.tsx
+++ src/components/sub-components/Services/Servicecard.tsx
@@ -1,15 +1,22 @@
-
+import web from '../../../Images/web.svg'
+import data from '../../../Images/data.svg'
+import seo from '../../../Images/seo.svg'
+import mobile from '../../../Images/mobile.svg'
 type o={
     id:number,
-    text:string
+    text:string,
+    image:string
 }
 const Servicecard = ({services}:any) => {
+    const imgs=[web,mobile,data,seo]
 
     return (
-     services.map((m:o)=>{
+     services.map((m:o,i: number)=>{
          return(
              <div className={`service`} key={m.id}>
                  <p>{m.text}</p>
+                 {console.log(i)}
+                 <img src={imgs[i]} alt="Illustration" loading="lazy"></img>
              </div>
          )
      })
